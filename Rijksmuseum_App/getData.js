0b5920080ff86b736416fa64bec0812066132699
Jeany de Vries <jeanydevries@hotmail.com>
2022-02-14 15:10:56 +0100

    display li in grid

--- Rijksmuseum_App/getData.js
+++ Rijksmuseum_App/getData.js
@@ -17,8 +17,12 @@ function getData(url)
         for (let i = 0; i < collection.artObjects.length; i++) {
             list.insertAdjacentHTML(
                 "beforebegin",
-                `<li><h2>${collection.artObjects[i].longTitle}</h2><img src="${collection.artObjects[i].webImage.url}" alt="${collection.artObjects[i].title}"/></li>`)
-        }
+                `<li>
+                    <h2>${collection.artObjects[i].title}</h2>4
+                    <p>${collection.artObjects[i].element}</p>
+                    <img src="${collection.artObjects[i].webImage.url}" alt="${collection.artObjects[i].title}"/>
+                </li>`)
+            }
     })
     .catch(function(error) {
         console.log(error);
