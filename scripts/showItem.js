f98bbca258b1d82b581fbdeb78ef29b4c7bbc29d
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-01 16:34:40 +0100

    image shows when clicked

--- scripts/showItem.js
+++ scripts/showItem.js
@@ -1,19 +1,43 @@
-import { $ } from "./getElement.js";
 import { dataMuseum } from "./getItemData.js";
 import { getDataID } from "./getItemData.js";
-
-const list = $('div');
 
 export function showItem(id, object){
     getDataID(id);
     console.log(dataMuseum)
-    zoomIn(object);
+    popUp(object);
 }
 
-function zoomIn(object){
-   // list.insertAdjacentHTML(
-     //  "beforebegin",
-    //    `<button class="displayedItem">
-    //    <h2>${dataMuseum.artObject.title}</h2>
-    //    </button>`)
+function popUp(object){
+    var artPiece = document.getElementById("artPiece");
+    var span = document.getElementsByClassName("close")[0];
+
+    object.style.scale = (1, 1);
+    artPiece.style.display = "block";
+
+    //When the button or outside is clicked, the display will be hidden
+    span.onclick = function() {
+        artPiece.style.display = "none";
+    }
+    window.onclick = function(event) 
+    {
+        if (event.target == artPiece) {
+            artPiece.style.display = "none";
+        }
+    }
+
+    displayContent();
+}
+
+var artPieceContent = document.getElementById("artPiece-content");
+function displayContent(){
+
+    //display content
+    artPieceContent.insertAdjacentHTML(
+        "afterbegin",
+        `<img src="${dataMuseum.artObject.webImage.url}" alt="${dataMuseum.artObject.title}"/>
+        <h2>${dataMuseum.artObject.title}</h2>`)
+}
+
+function removeContent(){
+
 }
