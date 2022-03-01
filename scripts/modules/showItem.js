e0762214c43b09ce9797af6eff4b7633f77dd751
Jeany <jeanydevries@hotmail.com>
2022-03-01 19:10:00 +0100

    remove content when out of pop up, but id is one behind NEED TO FIX

--- scripts/modules/showItem.js
+++ scripts/modules/showItem.js
@@ -3,7 +3,6 @@ import { getDataID } from "./getItemData.js";
 
 export function showItem(id, object){
     getDataID(id);
-    console.log(dataMuseum)
     popUp(object);
 }
 
@@ -17,27 +16,32 @@ function popUp(object){
     //When the button or outside is clicked, the display will be hidden
     span.onclick = function() {
         artPiece.style.display = "none";
+        removeContent();
     }
     window.onclick = function(event) 
     {
         if (event.target == artPiece) {
             artPiece.style.display = "none";
+            removeContent();
         }
     }
 
     displayContent();
 }
 
-var artPieceContent = document.getElementById("artPiece-content");
+var artPieceContent = document.getElementById("content");
 function displayContent(){
 
     //display content
     artPieceContent.insertAdjacentHTML(
-        "afterbegin",
+        "beforeend",
         `<img src="${dataMuseum.artObject.webImage.url}" alt="${dataMuseum.artObject.title}"/>
-        <h2>${dataMuseum.artObject.title}</h2>`)
+        <h2>${dataMuseum.artObject.title}</h2>
+        <p>${dataMuseum.artObject.description}</p>`)
 }
 
 function removeContent(){
-
+    while (artPieceContent.firstChild) {
+        artPieceContent.removeChild(artPieceContent.firstChild);
+    }
 }
